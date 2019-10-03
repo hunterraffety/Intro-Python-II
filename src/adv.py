b4a50f239645fdf9705f509f84196468800dc822
Hunter Raffety <hsraffety@gmail.com>
2019-10-03 16:22:39 -0700

    There are items in rooms

--- src/adv.py
+++ src/adv.py
@@ -6,21 +6,21 @@ from item import Item
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-"North of you, the cave mount beckons"),
+"North of you, the cave mount beckons", "Empty"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", "Sword"),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", "Candle"),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", "Cloak"),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", "Sadness"),
 }
 
 
@@ -41,7 +41,7 @@ room['treasure'].s_to = room['narrow']
 
 import textwrap
 #
-player = Player("Louise", room['outside'])
+player = Player("Louise", room['outside'], None)
 
 
 playerInput = ""
--- src/room.py
+++ src/room.py
@@ -2,10 +2,10 @@
 # description attributes.
 
 class Room:
-	def __init__(self, room_name, room_description, room_items):
+	def __init__(self, room_name, room_description, items):
 		self.room_name = room_name
 		self.room_description = room_description
-		self.room_items = room_items
+		self.items = items
 
 	def __str__(self):
-		return f"{self.room_name}. {self.room_description}"
+		return f"{self.room_name}. {self.room_description}. {self.items}"
