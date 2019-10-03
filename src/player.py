9793e01ff575723b02953754104773f1ac875071
Hunter Raffety <hsraffety@gmail.com>
2019-10-03 12:03:01 -0700

    cleaning up code to become more explicit

--- src/player.py
+++ src/player.py
@@ -1,8 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, room):
-        self.name = name
-        self.room = room
+    def __init__(self, player_name, current_room):
+        self.player_name = player_name
+        self.current_room = current_room
     def __str__(self):
-        return f"{self.room}"
+        return f"{self.current_room}"
--- src/room.py
+++ src/room.py
@@ -2,9 +2,9 @@
 # description attributes.
 
 class Room:
-	def __init__(self, name, description):
-		self.name = name
-		self.description = description
+	def __init__(self, room_name, room_description):
+		self.room_name = room_name
+		self.room_description = room_description
 
 	def __str__(self):
-		return f"{self.name}. {self.description}"
+		return f"{self.room_name}. {self.room_description}"
